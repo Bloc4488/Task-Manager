533f03705cf54cd5fb30354ebd58ad470fca3fdc
Bloc4488 <bloc4488@gmail.com>
2025-08-11 01:06:49 +0200

    feat: admin dashboard(full)

--- src/main/java/com/example/task_manager/dto/UserResponse.java
+++ src/main/java/com/example/task_manager/dto/UserResponse.java
@@ -1,4 +1,14 @@
 package com.example.task_manager.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class UserResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
 }
