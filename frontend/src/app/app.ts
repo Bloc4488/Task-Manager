93686765c4386518ea2b1221ba8e3164678d56db
Bloc4488 <bloc4488@gmail.com>
2025-08-11 23:11:51 +0200

    tmp: saving before merge

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from './shared/auth.service';
+import { LoadingService } from './shared/loading.service';
+import { ThemeService } from './shared/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,13 @@ import { AuthService } from './shared/auth.service';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  constructor(public authService: AuthService) {}
+export class App implements OnInit {
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    public themeService: ThemeService,
+    public loadingService: LoadingService
+  ) {}
+
+
 }
